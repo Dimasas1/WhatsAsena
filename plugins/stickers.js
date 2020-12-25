eb2c09c9c080048a31d3139bfdd2653fdc3d2b76
Dimasas1 <69768749+Dimasas1@users.noreply.github.com>
2020-12-25 09:41:12 +0700

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -18,7 +18,8 @@ const Lang = Language.getString('sticker');
 
 Asena.addCommand({pattern: 'sticker', fromMe: true, desc: Lang.STICKER_DESC}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);
-    var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
+    var downloading = await message.reply(Lang.DOWNLOADING);
+    
     var location = await message.client.downloadAndSaveMediaMessage({
         key: {
             remoteJid: message.reply_message.jid,
@@ -35,7 +36,7 @@ Asena.addCommand({pattern: 'sticker', fromMe: true, desc: Lang.STICKER_DESC}, (a
         
             await message.sendMessage(fs.readFileSync('./output.webp'), MessageType.sticker);
         });
-        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+        return await downloading.delete();
     }
 
     ffmpeg(location)
@@ -44,5 +45,5 @@ Asena.addCommand({pattern: 'sticker', fromMe: true, desc: Lang.STICKER_DESC}, (a
         .on('end', async () => {
             await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
         });
-    return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    return await downloading.delete();
 }));
